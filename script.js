b25b7c677e8c109e8b2dfda963558e9ed7435ec5
HEran-ite <herieshetu@gmail.com>
2024-11-15 12:59:29 +0300

    responsive

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener("click", function(e) {
-            e.preventDefault();
-            document.querySelector(this.getAttribute("href")).scrollIntoView({
-                behavior: "smooth"
-            });
-        });
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth",
+      });
     });
+  });
 
-    const form = document.querySelector("form");
-    form.addEventListener("submit", function(e) {
-        e.preventDefault();
-        const formData = new FormData(form);
-        fetch(form.action, {
-            method: form.method,
-            headers: {
-                'Accept': 'application/json'
-            },
-            body: formData
-        }).then(response => {
-            if (response.ok) {
-                alert("Thank you for contacting me!");
-                form.reset();
-            } else {
-                alert("Oops! There was a problem submitting your form");
-            }
-        }).catch(error => {
-            alert("Oops! There was a problem submitting your form");
-        });
-    });
+  const form = document.querySelector("form");
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const formData = new FormData(form);
+    fetch(form.action, {
+      method: form.method,
+      headers: {
+        Accept: "application/json",
+      },
+      body: formData,
+    })
+      .then((response) => {
+        if (response.ok) {
+          alert("Thank you for contacting me!");
+          form.reset();
+        } else {
+          alert("Oops! There was a problem submitting your form");
+        }
+      })
+      .catch((error) => {
+        alert("Oops! There was a problem submitting your form");
+      });
+  });
 });
 
+function toggleMenu() {
+  const navLinks = document.querySelector(".nav-links");
+  navLinks.classList.toggle("active");
+}
